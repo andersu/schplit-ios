f69c2f22fbaab99206b85645c758572453bab549
Anders Ullnæss <andersullnass@gmail.com>
2016-10-21 22:27:31 +0200

    Added CocoaPods and NibDesignable pod

---
+++ Schplit/Pods/Target Support Files/Pods-Schplit/Pods-Schplit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Schplit : NSObject
+@end
+@implementation PodsDummy_Pods_Schplit
+@end
---
+++ Schplit/Pods/Target Support Files/Pods-SchplitTests/Pods-SchplitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SchplitTests : NSObject
+@end
+@implementation PodsDummy_Pods_SchplitTests
+@end
