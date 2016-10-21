360eaf25050e67cacc0ee39a90534fa9228c8031
Anders Ullnæss <andersullnass@gmail.com>
2016-10-21 20:24:52 +0200

    Initial commit

---
+++ Schplit/Schplit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schplit
+//
+//  Created by Anders Ullnæss on 21/10/16.
+//  Copyright © 2016 Anders Ullnæss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
